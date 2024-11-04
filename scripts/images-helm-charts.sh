841fbd8af590bc649a62fde8c3fc24d71305c422
Vedran Pugar <vpugar@digital.ai>
2024-11-04 08:21:59 +0100

    S-112706 list all images during build (4)

--- scripts/images-helm-charts.sh
+++ scripts/images-helm-charts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#docker run --rm \
-#  -e HOME=/opt/project \
-#  -v ./:/opt/project:rw \
-#  --entrypoint helm \
-#  xldevdocker/kuttl:latest \
-#  dependency update
+docker run --rm \
+  -e HOME=/opt/project \
+  -v ./:/opt/project:rw \
+  --entrypoint helm \
+  xldevdocker/kuttl:latest \
+  dependency update
 
 docker run --rm \
   -v ./:/opt/project:rw \
