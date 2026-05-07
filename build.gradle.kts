107baafbfd5cb4aa9011740323b555a1c21dad09
Venkatesh N <venkatesh.nandakumar@digital.ai>
2026-05-07 16:03:36 +0530

    S-132940 Fix helm lint task and changed node label

--- build.gradle.kts
+++ build.gradle.kts
@@ -285,7 +285,7 @@ tasks {
         dependsOn("prepareHelmDeps")
 
         workingDir(buildXldOperatorDir)
-        commandLine(helmCli, "lint", "-f", "../../../tests/values/basic.yaml")
+        commandLine(helmCli, "lint", ".", "-f", "../../../tests/values/basic.yaml")
 
         doLast {
             logger.lifecycle("Finished running helm lint")
